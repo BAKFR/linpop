41805de676f741b4988cc1231cc3bb3e8b76891f
Kévin Bernard-Allies <kevin.bernard.allies@tumbleweed-studio.net>
2013-03-04 18:30:55 +0800

    uploadWindow

--- Linpop/main.cpp
+++ Linpop/main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include "loginwindow.h"
 #include "networkobject.h"
+#include "conversationwindow.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,8 @@ int main(int argc, char *argv[])
    w.show();
    NetworkObject no(&w);
     no.initialize(5000);
+    ConversationWindow cw;
+    cw.show();
 
     return a.exec();
 }
--- Linpop/uploadwindow.cpp
+++ Linpop/uploadwindow.cpp
@@ -13,6 +13,8 @@ UploadWindow::UploadWindow(QWidget *parent, const QString &file) :
     ft_upload = new FileTransfertUpload(info);
     ui->setupUi(this);
     ui->label_fileName->setText(info.fileName() + " (" + QString::number(info.size()) + " octets)");
+    ui->label_success->setVisible(false);
+    ui->label_error->setVisible(false);
 }
 
 UploadWindow::~UploadWindow()
@@ -23,7 +25,9 @@ UploadWindow::~UploadWindow()
 
 void    UploadWindow::onUploadComplete(int nb_ok, int nb_error, int nb_total)
 {
-    //TODO: MAJ
+    onUploadProgress(nb_ok, nb_error, nb_total);
+    //ui->info_layout->setVisible(false);
+    ui->label_success->setVisible(true);
     ui->buttonBox->setStandardButtons(QDialogButtonBox::Close);
 }
 
@@ -34,8 +38,10 @@ void    UploadWindow::onUploadProgress(int nb_ok, int nb_error, int nb_total)
 
 void    UploadWindow::onUploadError(const QString &error)
 {
+    ui->info_layout->widget()->setVisible(false);
+    ui->label_error->setVisible(true);
+    ui->label_error->setText(ui->label_error->text() + "\n" + error);
     ui->buttonBox->setStandardButtons(QDialogButtonBox::Close);
-    //TODO MAJ
 }
 
 void    UploadWindow::onClientError(const QString &error)
